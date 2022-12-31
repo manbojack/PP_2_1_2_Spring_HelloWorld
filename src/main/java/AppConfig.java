d2544cf4e3da714a12bb56dea9a0b80737fa48c0
Anton Akhramovich <akhramovich.anton@gmail.com>
2022-12-31 05:43:21 +0300

    apply code style

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
